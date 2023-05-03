a3063cdc2e7f2da6526d41a005c7829db69455ed
kizo-88 <118953087+kizo-88@users.noreply.github.com>
2023-05-03 16:10:37 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-ozxggu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-ozxggu)
---
+++ client/tailwind.config.js
@@ -0,0 +1,10 @@
+import forms from '@tailwindcss/forms';
+
+module.exports = {
+  content: [
+    './components/**/*.{html,js}',
+    './pages/**/*.{html,js}',
+    './index.html',
+  ],
+  // ...
+}
